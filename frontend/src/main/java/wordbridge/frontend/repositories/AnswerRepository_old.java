bf44f1e93c36415e7753b7ef8d348c6f9a99b645
Karl <beckka8818@uwec.edu>
2026-03-11 13:51:39 -0500

    deleted old files

--- frontend/src/main/java/wordbridge/frontend/repositories/AnswerRepository_old.java
+++
@@ -1,7 +0,0 @@
-package wordbridge.frontend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import wordbridge.frontend.models.Answer_old;
-public interface AnswerRepository_old extends JpaRepository<Answer_old, Long> {
-}
--- frontend/src/main/java/wordbridge/frontend/repositories/UserRepository_old.java
+++
@@ -1,7 +0,0 @@
-package wordbridge.frontend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import wordbridge.frontend.models.User_old;
-public interface UserRepository_old extends JpaRepository<User_old, Long> {
-}
